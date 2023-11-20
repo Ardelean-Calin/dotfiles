d90fca8e6ce39c3ce3a56eee9e9fb158496d2055
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2023-11-20 20:57:19 +0200

    Added wal.

--- home/home.nix
+++ home/home.nix
@@ -460,24 +460,9 @@ in {
     recursive = true;
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/dots/helix";
   };
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # ".config/wezterm" = {
-    #   recursive = true;
-    #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/dots/wezterm";
-    # };
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  xdg.configFile."wal" = {
+    recursive = true;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/dots/wal";
   };
 
   # You can also manage environment variables but you will have to manually
